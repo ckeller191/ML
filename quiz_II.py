3d279e1edd34e690de5c44a2a3b50c834dc4f8a1
Chris Keller <ckeller191@gmail.com>
2021-04-14 13:16:15 -0500

    done with quiz 2

--- quiz_II.py
+++ quiz_II.py
@@ -1,5 +1,40 @@
 import pandas as pd
 
-classes_df = pd.read_csv(r'animal_classes.csv')
+classes = pd.read_csv('animal_classes.csv')
 
-print(classes_df)
+train = pd.read_csv('animals_train.csv')
+
+test = pd.read_csv('animals_test.csv')
+
+
+train_copy = train.copy()
+train_data = train_copy.drop(columns= ["class_number"])
+
+test_copy = test.copy()
+test_data = test_copy.drop(columns= ["animal_name"])
+
+target_train = train["class_number"]
+
+from sklearn.neighbors import KNeighborsClassifier
+
+
+knn = KNeighborsClassifier()
+
+knn.fit(X=train_data, y=target_train)
+
+predicted = knn.predict(X=test_data)
+
+
+animalnames = test["animal_name"]
+
+class_prediction = [classes["Class_Type"][classes["Class_Number"] == i].values[0] for i in predicted]
+
+
+final_df = pd.DataFrame({'animal_nar':animalnames, 'prediction':class_prediction})
+
+final_df.to_csv("predictions.csv", index=False, sep=' ')
+
+
+
+
+
